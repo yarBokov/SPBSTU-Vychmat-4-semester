b736d722e549cfff06047628e63e89da9e55081a
Yaroslav <121685813+yarBokov@users.noreply.github.com>
2023-02-04 18:48:09 +0300

    Fixed spline.c

--- Lab1/spline.c
+++ Lab1/spline.c
@@ -1,57 +1,69 @@
+/* spline.c
+   Cubic interpolating spline. */
 
-#include <math.h>
+   /************************************************/
+   /*                                              */
+   /*  CMATH.  Copyright (c) 1989 Design Software  */
+   /*                                              */
+   /************************************************/
+
 #include "cmath.h"
 
 /*-----------------------------------------------------------------*/
 
 #if (PROTOTYPE)
 
-int quanc8 (double (*fun)(double x), double a, double b,
-            double abserr, double relerr,
-            double *resultR, double *errestR,
-            int *nofunR,
-            double *posnR, int *flag)
+int spline(int n, int end1, int end2,
+  double slope1, double slope2,
+  double x[], double y[],
+  double b[], double c[], double d[],
+  int* iflag)
 
 #else
 
-int quanc8 (fun, a, b, abserr, relerr, resultR, errestR,
-            nofunR, posnR, flag)
-double (*fun)();
-double a, b, abserr, relerr;
-double *resultR, *errestR, *posnR;
-int    *nofunR, *flag;
+int spline(n, end1, end2, slope1, slope2,
+  x, y, b, c, d, iflag)
+
+  int    n, end1, end2;
+double slope1, slope2;
+double x[], y[], b[], c[], d[];
+int* iflag;
 
 #endif
 
-/*  Purpose ...
-    -------
-    Estimate the integral of fun(x) from a to b to a user-
-    specified tolerance.  This is an automatic adaptive
-    routine based on the 8-panel Newton-Cotes rule.
 
-    Input ...
-    -----
-       fun : the name of the integrand function fun(x). (pointer)
-         a : the lower limit of integration.
-	 b : the upper limit of integration. (b may be less than a.)
-    relerr : a relative error tolerance. (should be non-negative)
-    abserr : an absolute error tolerance. (should be non-negative)
+/* Purpose ...
+   -------
+   Evaluate the coefficients b[i], c[i], d[i], i = 0, 1, .. n-1 for
+   a cubic interpolating spline
 
-    Output ... pointers to ...
-    ------
-    result : an approximation to the integral, hopefully satisfying the
-             least stringent of the two error tolerances.
-    errest : an estimate of the magnitude of the actual error.
-     nofun : the number of function values used in calculation of result
-      flag : Status indicator.
-             = 0, normal return.
-             = 1, illegal user input, relerr < 0.0, abserr < 0.0
-             < 0, if flag = -n, then n subintervals have not converged
-                  and the result is unreliable.  A small number of
-                  unconverged subintervals may be acceptable.
-      posn : The problem spot. If posn is zero, then result
-             probably satisfies the error tolerance. If flag < 0, posn is
-             the point reached when the limit on nofun was approached.
+   S(xx) = Y[i] + b[i] * w + c[i] * w**2 + d[i] * w**3
+   where w = xx - x[i]
+   and   x[i] <= xx <= x[i+1]
+
+   The n supplied data points are x[i], y[i], i = 0 ... n-1.
+
+   Input :
+   -------
+   n       : The number of data points or knots (n >= 2)
+   end1,
+   end2    : = 1 to specify the slopes at the end points
+             = 0 to obtain the default conditions
+   slope1,
+   slope2  : the slopes at the end points x[0] and x[n-1]
+             respectively
+   x[]     : the abscissas of the knots in strictly
+             increasing order
+   y[]     : the ordinates of the knots
+
+   Output :
+   --------
+   b, c, d : arrays of spline coefficients as defined above
+             (See note 2 for a definition.)
+   iflag   : status flag
+            = 0 normal return
+            = 1 less than two data points; cannot interpolate
+            = 2 x[] are not in ascending order
 
    This C code written by ...  Peter & Nigel,
    ----------------------      Design Software,
@@ -59,218 +71,408 @@ int    *nofunR, *flag;
                                Kenmore, 4069,
                                Australia.
 
-    Version .. 1.0, October 1987
-    -------    1.1, March   1989
-               1.2, July    1989  integer flag added.
-               1.3, Sept    1989  flag and posn modified
+   Version ... 1.1, 30 September 1987
+   -------     2.0, 6 April 1989    (start with zero subscript)
+                                     remove ndim from parameter list
+               2.1, 28 April 1989   (check on x[])
+               2.2, 10 Oct   1989   change number order of matrix
 
-    Notes ...
-    -----
-    (1) This routine has been adapted from that in the text
-        G.E. Forsythe, M.A. Malcolm & C.B. Moler
-        "Computer methods for mathematical computations"
-        Prentice-Hall, Englewood Cliffs, N.J. (1977)
+   Notes ...
+   -----
+   (1) The accompanying function seval() may be used to evaluate the
+       spline while deriv will provide the first derivative.
+   (2) Using p to denote differentiation
+       y[i] = S(X[i])
+       b[i] = Sp(X[i])
+       c[i] = Spp(X[i])/2
+       d[i] = Sppp(X[i])/6  ( Derivative from the right )
+   (3) Since the zero elements of the arrays ARE NOW used here,
+       all arrays to be passed from the main program should be
+       dimensioned at least [n].  These routines will use elements
+       [0 .. n-1].
+   (4) Adapted from the text
+       Forsythe, G.E., Malcolm, M.A. and Moler, C.B. (1977)
+       "Computer Methods for Mathematical Computations"
+       Prentice Hall
+   (5) Note that although there are only n-1 polynomial segments,
+       n elements are requird in b, c, d.  The elements b[n-1],
+       c[n-1] and d[n-1] are set to continue the last segment
+       past x[n-1].
+*/
+
+/*----------------------------------------------------------------*/
+
+{  /* begin procedure spline() */
+
+  int    nm1, ib, i;
+  double t;
+  int    ascend;
+
+  nm1 = n - 1;
+  *iflag = 0;
+
+  if (n < 2)
+  {  /* no possible interpolation */
+    *iflag = 1;
+    goto LeaveSpline;
+  }
+
+  ascend = 1;
+  for (i = 1; i < n; ++i) if (x[i] <= x[i - 1]) ascend = 0;
+  if (!ascend)
+  {
+    *iflag = 2;
+    goto LeaveSpline;
+  }
+
+  if (n >= 3)
+  {    /* ---- At least quadratic ---- */
+
+  /* ---- Set up the symmetric tri-diagonal system
+          b = diagonal
+          d = offdiagonal
+          c = right-hand-side  */
+    d[0] = x[1] - x[0];
+    c[1] = (y[1] - y[0]) / d[0];
+    for (i = 1; i < nm1; ++i)
+    {
+      d[i] = x[i + 1] - x[i];
+      b[i] = 2.0 * (d[i - 1] + d[i]);
+      c[i + 1] = (y[i + 1] - y[i]) / d[i];
+      c[i] = c[i + 1] - c[i];
+    }
+
+    /* ---- Default End conditions
+            Third derivatives at x[0] and x[n-1] obtained
+            from divided differences  */
+    b[0] = -d[0];
+    b[nm1] = -d[n - 2];
+    c[0] = 0.0;
+    c[nm1] = 0.0;
+    if (n != 3)
+    {
+      c[0] = c[2] / (x[3] - x[1]) - c[1] / (x[2] - x[0]);
+      c[nm1] = c[n - 2] / (x[nm1] - x[n - 3]) - c[n - 3] / (x[n - 2] - x[n - 4]);
+      c[0] = c[0] * d[0] * d[0] / (x[3] - x[0]);
+      c[nm1] = -c[nm1] * d[n - 2] * d[n - 2] / (x[nm1] - x[n - 4]);
+    }
+
+    /* Alternative end conditions -- known slopes */
+    if (end1 == 1)
+    {
+      b[0] = 2.0 * (x[1] - x[0]);
+      c[0] = (y[1] - y[0]) / (x[1] - x[0]) - slope1;
+    }
+    if (end2 == 1)
+    {
+      b[nm1] = 2.0 * (x[nm1] - x[n - 2]);
+      c[nm1] = slope2 - (y[nm1] - y[n - 2]) / (x[nm1] - x[n - 2]);
+    }
+
+    /* Forward elimination */
+    for (i = 1; i < n; ++i)
+    {
+      t = d[i - 1] / b[i - 1];
+      b[i] = b[i] - t * d[i - 1];
+      c[i] = c[i] - t * c[i - 1];
+    }
+
+    /* Back substitution */
+    c[nm1] = c[nm1] / b[nm1];
+    for (ib = 0; ib < nm1; ++ib)
+    {
+      i = n - ib - 2;
+      c[i] = (c[i] - d[i] * c[i + 1]) / b[i];
+    }
+
+    /* c[i] is now the sigma[i] of the text */
+
+    /* Compute the polynomial coefficients */
+    b[nm1] = (y[nm1] - y[n - 2]) / d[n - 2] + d[n - 2] * (c[n - 2] + 2.0 * c[nm1]);
+    for (i = 0; i < nm1; ++i)
+    {
+      b[i] = (y[i + 1] - y[i]) / d[i] - d[i] * (c[i + 1] + 2.0 * c[i]);
+      d[i] = (c[i + 1] - c[i]) / d[i];
+      c[i] = 3.0 * c[i];
+    }
+    c[nm1] = 3.0 * c[nm1];
+    d[nm1] = d[n - 2];
+
+  }  /* at least quadratic */
+
+  else  /* if n >= 3 */
+  {  /* linear segment only  */
+    b[0] = (y[1] - y[0]) / (x[1] - x[0]);
+    c[0] = 0.0;
+    d[0] = 0.0;
+    b[1] = b[0];
+    c[1] = 0.0;
+    d[1] = 0.0;
+  }
+
+LeaveSpline:
+  return 0;
+}  /* end of spline() */
+/*-------------------------------------------------------------------*/
+
+#if (PROTOTYPE)
+
+double seval(int n, double u,
+  double x[], double y[],
+  double b[], double c[], double d[],
+  int* last)
+
+#else
+
+double seval(n, u, x, y, b, c, d, last)
+
+int    n;
+double u;
+double x[], y[], b[], c[], d[];
+int* last;
+
+#endif
+
+/*Purpose ...
+  -------
+  Evaluate the cubic spline function
+
+  S(xx) = y[i] + b[i] * w + c[i] * w**2 + d[i] * w**3
+  where w = u - x[i]
+  and   x[i] <= u <= x[i+1]
+  Note that Horner's rule is used.
+  If u < x[0]   then i = 0 is used.
+  If u > x[n-1] then i = n-1 is used.
+
+  Input :
+  -------
+  n       : The number of data points or knots (n >= 2)
+  u       : the abscissa at which the spline is to be evaluated
+  Last    : the segment that was last used to evaluate U
+  x[]     : the abscissas of the knots in strictly increasing order
+  y[]     : the ordinates of the knots
+  b, c, d : arrays of spline coefficients computed by spline().
+
+  Output :
+  --------
+  seval   : the value of the spline function at u
+  Last    : the segment in which u lies
+
+  Notes ...
+  -----
+  (1) If u is not in the same interval as the previous call then a
+      binary search is performed to determine the proper interval.
 
 */
-/*-----------------------------------------------------------------*/
+/*-------------------------------------------------------------------*/
 
-{  /* begin function quanc8 */
+{  /* begin function seval() */
 
-/* this code has been translated from fortran, hence use
-   elements 1 .. n */
-double w0, w1, w2, w3, w4, area, x0, f0, stone, step, cor11;
-double qprev, qnow, qdiff, qleft, esterr, tolerr;
-static double qright[32], f[17], x[17], fsave[9][31];
-static double xsave[9][31];
-double posn, result, errest;
-double temp, temp1;
-int nofun;
-int levmin, levmax, levout, nomax, nofin, lev, nim, i, j, ii;
+  int    i, j, k;
+  double w;
 
-/* check user input */
-if (abserr < 0.0 || relerr < 0.0)
-   {
-   *flag = 1;
-   *resultR = 0.0;
-   *errestR = 0.0;
-   *posnR   = 0.0;
-   *nofunR  = 0;
-   return (0);
-   }
+  i = *last;
+  if (i >= n - 1) i = 0;
+  if (i < 0)  i = 0;
 
-/*  ***  stage 1 ***  general initialization
-    set constants.  */
-levmin = 1;
-levmax = 30;
-levout = 6;
-nomax = 5000;
+  if ((x[i] > u) || (x[i + 1] < u))
+  {  /* ---- perform a binary search ---- */
+    i = 0;
+    j = n;
+    do
+    {
+      k = (i + j) / 2;         /* split the domain to search */
+      if (u < x[k])  j = k;    /* move the upper bound */
+      if (u >= x[k]) i = k;    /* move the lower bound */
+    }                        /* there are no more segments to search */
+    while (j > i + 1);
+  }
+  *last = i;
 
-{
-ii = 1;
-for (i = 0; i <= levout; i++) ii *= 2;
-} /* ---> ii = 2 ** (levout+1) */
-nofin = nomax - 8 * (levmax - levout + ii);
-/* note that there will be trouble when nofun reaches nofin */
+  /* ---- Evaluate the spline ---- */
+  w = u - x[i];
+  w = y[i] + w * (b[i] + w * (c[i] + w * d[i]));
+  return (w);
+}
+/*-------------------------------------------------------------------*/
 
-temp =  14175.0;
-w0   =   3956.0 / temp;
-w1   =  23552.0 / temp;
-w2   =  -3712.0 / temp;
-w3   =  41984.0 / temp;
-w4   = -18160.0 / temp;
+#if (PROTOTYPE)
 
-/* initialize running sums to zero. */
-*flag  = 0;
-posn   = 0.0;
-result = 0.0;
-cor11  = 0.0;
-errest = 0.0;
-area   = 0.0;
-nofun  = 0;
-if (a == b) goto ExitQuanc8;
+double deriv(int n, double u,
+  double x[],
+  double b[], double c[], double d[],
+  int* last)
 
-/* ***  stage 2 ***  initialization for first interval */
-lev   = 0;
-nim   = 1;
-x0    = a;
-x[16] = b;
-qprev = 0.0;
+#else
 
-/* set up evenly spaced panels */
-f0    = (*fun) (x0);
-stone = (b - a) / 16.0;
-x[8]  = 0.5 * (x0 + x[16]);
-x[4]  = 0.5 * (x0 + x[8]);
-x[12] = 0.5 * (x[8] + x[16]);
-x[2]  = 0.5 * (x0 + x[4]);
-x[6]  = 0.5 * (x[4] + x[8]);
-x[10] = 0.5 * (x[8] + x[12]);
-x[14] = 0.5 * (x[12] + x[16]);
-for (j = 2; j <= 16; j = j + 2)  f[j] = (*fun) (x[j]);
-nofun = 9;
+double deriv(n, u, x, b, c, d, last)
 
-/* ***  stage 3 ***  central calculation
-   requires qprev,x0,x2,x4,...,x16,f0,f2,f4,...,f16.
-   calculates x1,x3,...x15, f1,f3,...f15,qleft,qright,qnow,qdiff,area.
+int    n;
+double u;
+double x[], b[], c[], d[];
+int* last;
+
+#endif
+
+/* Purpose ...
+   -------
+   Evaluate the derivative of the cubic spline function
+
+   S(x) = B[i] + 2.0 * C[i] * w + 3.0 * D[i] * w**2
+   where w = u - X[i]
+   and   X[i] <= u <= X[i+1]
+   Note that Horner's rule is used.
+   If U < X[0] then i = 0 is used.
+   If U > X[n-1] then i = n-1 is used.
+
+   Input :
+   -------
+   n       : The number of data points or knots (n >= 2)
+   u       : the abscissa at which the derivative is to be evaluated
+   last    : the segment that was last used
+   x       : the abscissas of the knots in strictly increasing order
+   b, c, d : arrays of spline coefficients computed by spline()
+
+   Output :
+   --------
+   deriv : the value of the derivative of the spline
+           function at u
+   last  : the segment in which u lies
+
+   Notes ...
+   -----
+   (1) If u is not in the same interval as the previous call then a
+       binary search is performed to determine the proper interval.
+
 */
-Stage3:
-x[1] = 0.5 * (x0 + x[2]);
-f[1] = (*fun) (x[1]);
-for (j = 3; j <= 15; j = j + 2)
-   {
-   x[j] = 0.5 * (x[j-1] + x[j+1]);
-   f[j] = (*fun) (x[j]);
-   }
-nofun += 8;
-step  = (x[16] - x0) / 16.0;
-qleft = (w0 * (f0 + f[8]) + w1 * (f[1]+f[7]) + w2 * (f[2]+f[6])
-        + w3 * (f[3] + f[5]) + w4 * f[4]) * step;
-qright[lev+1] = (w0 * (f[8]+f[16]) + w1 * (f[9]+f[15]) + w2 * (f[10]+f[14])
-                + w3 * (f[11]+f[13]) + w4 * f[12]) * step;
-qnow  = qleft + qright[lev+1];
-qdiff = qnow - qprev;
-area += qdiff;
+/*-------------------------------------------------------------------*/
 
-/* ***  stage 4 *** interval convergence test */
-esterr = fabs(qdiff) / 1023.0;
-{
-tolerr = abserr;
-temp   = relerr * fabs(area);
-if (temp > tolerr) tolerr = temp;
-}  /* ---> tolerr = max(abserr, relerr * abs(area)) */
-tolerr *= (step / stone);
+{  /* begin function deriv() */
 
-if (lev < levmin)     goto Stage5;  /* keep subdividing               */
-if (lev >= levmax)    goto Stage6B; /* too many nested levels         */
-if (nofun > nofin)    goto Stage6;  /* close to limit on fn calls     */
-if (esterr <= tolerr) goto Stage7;  /* this interval has converged    */
+  int    i, j, k;
+  double w;
 
-/* ***  stage 5  ***  no convergence
-   locate next interval.             */
-Stage5:
-nim *= 2;
-++lev;
+  i = *last;
+  if (i >= n - 1) i = 0;
+  if (i < 0) i = 0;
 
-/* store right hand elements for future use. */
-for (i = 1; i <= 8; i++)
-   {
-   fsave[i][lev] = f[i+8];
-   xsave[i][lev] = x[i+8];
-   }
+  if ((x[i] > u) || (x[i + 1] < u))
+  {  /* ---- perform a binary search ---- */
+    i = 0;
+    j = n;
+    do
+    {
+      k = (i + j) / 2;          /* split the domain to search */
+      if (u < x[k])  j = k;     /* move the upper bound */
+      if (u >= x[k]) i = k;     /* move the lower bound */
+    }                         /* there are no more segments to search */
+    while (j > i + 1);
+  }
+  *last = i;
 
-/* assemble left hand elements for immediate use. */
-qprev = qleft;
-for (i = 1; i <= 8; i++)
-   {
-   j = (-i);
-   f[2 * j + 18] = f[j+9];
-   x[2 * j + 18] = x[j+9];
-   }
+  /* ---- Evaluate the derivative ---- */
+  w = u - x[i];
+  w = b[i] + w * (2.0 * c[i] + w * 3.0 * d[i]);
+  return (w);
 
-goto Stage3;
+} /* end of deriv() */
 
-/* ***  stage 6  ***  trouble section
-   number of function values is about to exceed limit.  */
-Stage6:
-nofin *= 2;
-levmax = levout;
-posn   = x0;   /* this is our trouble spot */
-goto Stage7;
+/*-------------------------------------------------------------------*/
 
-/* current level is levmax. */
-Stage6B:
---(*flag);   /* another interval has not converged */
+#if (PROTOTYPE)
 
-/* ***  stage 7  ***  interval converged
-   add contributions into running sums.  */
-Stage7:
-result += qnow;
-errest += esterr;
-cor11  += qdiff / 1023.0;
+double sinteg(int n, double u,
+  double x[], double y[],
+  double b[], double c[], double d[],
+  int* last)
 
-/* locate next interval. */
-while (nim != (2 * (nim/2)))
-   {
-   nim /= 2;
-   --lev;
-   }
-++nim;
-if (lev <= 0) goto Stage8;
+#else
 
-/* assemble elements required for the next interval. */
-qprev = qright[lev];
-x0    = x[16];
-f0    = f[16];
-for (i = 1; i <= 8; i++)
-   {
-   f[2 * i] = fsave[i][lev];
-   x[2 * i] = xsave[i][lev];
-   }
-goto Stage3;
+double sinteg(n, u, x, y, b, c, d, last)
 
-/* ***  stage 8  ***  finalize and return */
-Stage8:
-result += cor11;
+int    n;
+double u;
+double x[], y[], b[], c[], d[];
+int* last;
 
-/* make sure errest not less than roundoff level. */
-if (errest == 0.0) goto ExitQuanc8;
-temp1 = fabs(result);
-temp  = temp1 + errest;
-while (temp == temp1)
-   {
-   errest *= 2.0;
-   temp = temp1 + errest;
-   }
+#endif
 
-ExitQuanc8:
-/* --- return variables --- */
-*resultR = result;
-*errestR = errest;
-*posnR   = posn;
-*nofunR  = nofun;
-return (0);
-}  /* end of quanc8() */
+/*Purpose ...
+  -------
+  Integrate the cubic spline function
 
-/*-----------------------------------------------------------------*/
+  S(xx) = y[i] + b[i] * w + c[i] * w**2 + d[i] * w**3
+  where w = u - x[i]
+  and   x[i] <= u <= x[i+1]
 
+  The integral is zero at u = x[0].
 
+  If u < x[0]   then i = 0 segment is extrapolated.
+  If u > x[n-1] then i = n-1 segment is extrapolated.
+
+  Input :
+  -------
+  n       : The number of data points or knots (n >= 2)
+  u       : the abscissa at which the spline is to be evaluated
+  Last    : the segment that was last used to evaluate U
+  x[]     : the abscissas of the knots in strictly increasing order
+  y[]     : the ordinates of the knots
+  b, c, d : arrays of spline coefficients computed by spline().
+
+  Output :
+  --------
+  sinteg  : the value of the spline function at u
+  Last    : the segment in which u lies
+
+  Notes ...
+  -----
+  (1) If u is not in the same interval as the previous call then a
+      binary search is performed to determine the proper interval.
+
+*/
+/*-------------------------------------------------------------------*/
+
+{  /* begin function sinteg() */
+
+  int    i, j, k;
+  double sum, dx;
+
+  i = *last;
+  if (i >= n - 1) i = 0;
+  if (i < 0)  i = 0;
+
+  if ((x[i] > u) || (x[i + 1] < u))
+  {  /* ---- perform a binary search ---- */
+    i = 0;
+    j = n;
+    do
+    {
+      k = (i + j) / 2;         /* split the domain to search */
+      if (u < x[k])  j = k;    /* move the upper bound */
+      if (u >= x[k]) i = k;    /* move the lower bound */
+    }                        /* there are no more segments to search */
+    while (j > i + 1);
+  }
+  *last = i;
+
+  sum = 0.0;
+  /* ---- Evaluate the integral for segments x < u ---- */
+  for (j = 0; j < i; ++j)
+  {
+    dx = x[j + 1] - x[j];
+    sum += dx *
+      (y[j] + dx *
+        (0.5 * b[j] + dx *
+          (c[j] / 3.0 + dx * 0.25 * d[j])));
+  }
+
+  /* ---- Evaluate the integral fot this segment ---- */
+  dx = u - x[i];
+  sum += dx *
+    (y[i] + dx *
+      (0.5 * b[i] + dx *
+        (c[i] / 3.0 + dx * 0.25 * d[i])));
+
+  return (sum);
+}
+/*-------------------------------------------------------------------*/
